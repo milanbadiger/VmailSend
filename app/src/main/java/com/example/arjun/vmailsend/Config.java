9e35bff4b1551af941b5e43ee48e419d5febb097
Milan <milanbadiger@gmail.com>
2018-01-08 13:49:08 +0530

    test

---
+++ app/src/main/java/com/example/arjun/vmailsend/Config.java
@@ -0,0 +1,11 @@
+package com.example.arjun.vmailsend;
+
+/**
+ * Created by arjun on 12/19/2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="milanbadiger@gmail.com";
+    public static final String PASSWORD ="milanone";
+
+}
